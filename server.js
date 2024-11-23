50a2c023450a615aa6a36216b7341085589afa35
yerramurali <yerramurali52@gmail.com>
2024-11-23 09:11:21 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+  res.send('Employee Assistance Chatbot Backend');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
